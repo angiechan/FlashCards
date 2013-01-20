ac153d4bdf47d240a6abafc3c4255f571c3c57cd
yaelwb <yaelwb@gmail.com>
2013-01-20 11:31:44 -0800

    adding mobile directory

---
+++ mobile/FCAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  FCAppDelegate.h
+//  FlashCards
+//
+//  Created by Yael Weinberg on 1/19/13.
+//  Copyright (c) 2013 Yael Weinberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ mobile/FCWordsViewController.h
@@ -0,0 +1,13 @@
+//
+//  FCWordsViewController.h
+//  FlashCards
+//
+//  Created by Yael Weinberg on 1/19/13.
+//  Copyright (c) 2013 Yael Weinberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FCWordsViewController : UIViewController
+
+@end
